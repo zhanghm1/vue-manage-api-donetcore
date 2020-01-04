3674efe1cc8b3b5f51fcada1b4d850485c90a3f3
zhm <zhanghaiming_zhm@qq.com>
2020-01-04 09:53:08 +0800

    初始化项目

---
+++ VueManage.Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace VueManage.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiControllerBase : ControllerBase
+    {
+        //public readonly ILogger<ApiControllerBase> _logger;
+
+        //public ApiControllerBase(ILogger<ApiControllerBase> logger)
+        //{
+        //    _logger = logger;
+        //}
+    }
+}
